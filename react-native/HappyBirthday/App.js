6e3cb0526e3f71ed1ee6c6e93b19f6a62d8ba65f
szz <shizengzhou@outlook.com>
2019-11-26 13:46:40 +0800

    init

---
+++ react-native/HappyBirthday/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require('./assets/images/androidparty.jpg')}
+      />
+      <Text style={[styles.text, styles.topLeft]}>Happy Birthday, Tom</Text>
+      <Text style={[styles.text, styles.bottomRight]}>From, Jack</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+  },
+  text: {
+    color: '#fff',
+    fontFamily: 'sans-serif-light',
+    fontSize: 36,
+    padding: 20,
+    position: 'absolute',
+  },
+  topLeft: {
+    top: 0,
+    left: 0,
+  },
+  bottomRight: {
+    bottom: 0,
+    right: 0,
+  },
+});
+
+export default App;
