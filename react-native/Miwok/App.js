27e3cd3deed71f91f81b5aed131e67f53050a58a
szz <shizengzhou@outlook.com>
2020-01-17 22:44:27 +0800

    使用 TabView 重构

--- react-native/Miwok/App.js
+++ react-native/Miwok/App.js
@@ -1,33 +1,72 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import 'react-native-gesture-handler';
-import Home from './pages/Home';
+import React, {Component} from 'react';
+import {Dimensions, StyleSheet} from 'react-native';
+import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
 import Numbers from './pages/Numbers';
 import FamilyMembers from './pages/FamilMembers';
 import Colors from './pages/Colors';
 import Phrases from './pages/Phrases';
 
-const MainNavigator = createStackNavigator(
-  {
-    Home: {screen: Home, navigationOptions: () => ({title: 'Miwok'})},
-    Numbers: {screen: Numbers, navigationOptions: () => ({title: 'Numbers'})},
-    FamilyMembers: {
-      screen: FamilyMembers,
-      navigationOptions: () => ({title: 'Family Members'}),
-    },
-    Colors: {screen: Colors, navigationOptions: () => ({title: 'Colors'})},
-    Phrases: {screen: Phrases, navigationOptions: () => ({title: 'Phrases'})},
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: '#4a312a',
-      },
-      headerTintColor: '#fff',
-    },
-  },
-);
+const initialLayout = {width: Dimensions.get('window').width};
 
-const App = createAppContainer(MainNavigator);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      routes: [
+        {key: 'numbers', title: 'Numbers'},
+        {key: 'family', title: 'Family'},
+        {key: 'colors', title: 'Colors'},
+        {key: 'phrases', title: 'Phrases'},
+      ],
+    };
+    this.renderScene = SceneMap({
+      numbers: Numbers,
+      family: FamilyMembers,
+      colors: Colors,
+      phrases: Phrases,
+    });
+  }
+
+  handleIndexChange = index => {
+    this.setState({
+      index,
+    });
+  };
+
+  renderTabBar = props => (
+    <TabBar
+      {...props}
+      indicatorStyle={styles.indicator}
+      style={styles.tabBar}
+      labelStyle={styles.label}
+    />
+  );
+
+  render() {
+    return (
+      <TabView
+        navigationState={this.state}
+        renderScene={this.renderScene}
+        onIndexChange={this.handleIndexChange}
+        initialLayout={initialLayout}
+        renderTabBar={this.renderTabBar}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  indicator: {
+    backgroundColor: '#fff',
+  },
+  tabBar: {
+    backgroundColor: '#4a312a',
+  },
+  label: {
+    fontSize: 14,
+    textTransform: 'uppercase',
+  },
+});
 
 export default App;
